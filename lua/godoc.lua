5e5c1cf91424c56754fdd07ae0d257b440d60244
blacktrub <lightlocdog@gmail.com>
2023-04-25 21:56:37 +0300

    add base example telescope extenstion

--- lua/godoc.lua
+++ lua/godoc.lua
@@ -1,7 +1,18 @@
 local packages = require("packages")
 
 local function show_doc_page(ctx)
-	vim.cmd("tabnew | r ! go doc " .. ctx.args)
+	local buf = vim.api.nvim_create_buf(false, true)
+	vim.fn.jobstart({ "go", "doc", ctx.args }, {
+		stdout_buffered = true,
+		on_stdout = function(_, data)
+			if data then
+			  print(data, buf)
+				vim.api.nvim_buf_set_lines(buf, 0, -1, false, data)
+			end
+		end,
+	})
+
+	vim.api.buf(buf, true, {})
 end
 
 vim.api.nvim_create_user_command("GoDoc", show_doc_page, { nargs = 1 })
---
+++ lua/picker.lua
@@ -0,0 +1,41 @@
+local packages = require("packages")
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+local colors = function(opts)
+	opts = opts or {}
+	pickers
+		.new(opts, {
+			prompt_title = "colors",
+			finder = finders.new_table({
+				results = {
+					{ "red", "#ff0000" },
+					{ "green", "#00ff00" },
+					{ "blue", "#0000ff" },
+				},
+				entry_maker = function(entry)
+					return {
+						value = entry,
+						display = entry[1],
+						ordinal = entry[1],
+					}
+				end,
+			}),
+			sorter = conf.generic_sorter(opts),
+			attach_mappings = function(prompt_bufnr, map)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+					local selection = action_state.get_selected_entry()
+					-- print(vim.inspecredt(selection))
+					vim.api.nvim_put({ selection[1] }, "", false, true)
+				end)
+				return true
+			end,
+		})
+		:find()
+end
+
+colors(require("telescope.themes").get_dropdown({}))
